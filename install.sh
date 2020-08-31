b23597bfd006ff46df0174e034c468e315b8e4e3
Ryan <ryandanver@gmail.com>
2020-08-31 00:24:12 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# Set mish as a defualt shell
+
 INSTALL_DIR="/usr/bin"
 
 echo "Installing mish"
-cp ./mish "$INSTALL_DIR"
 
-if ! grep -Fxq "$INSTALL_DIR/mish" /etc/shells; then
-    echo "/usr/bin/mish" >> /etc/shells
+if [ -f "mish" ]; then
+    cp ./mish "$INSTALL_DIR"
+
+    if ! grep -Fxq "$INSTALL_DIR/mish" /etc/shells; then
+        echo "/usr/bin/mish" >> /etc/shells
+    fi
+else
+    echo "couldn't find executable"
 fi
