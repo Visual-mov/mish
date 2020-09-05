349d3cde4d6a69076383ba226eb644ac053f3b0a
Ryan <ryandanver@gmail.com>
2020-09-04 22:25:36 -0700

    work on lexer

--- src/lex.c
+++ src/lex.c
@@ -1,28 +1,42 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "lex.h"
 
 /* Mish lexer / tokenizer */
 
-typedef enum {
-    ARG, STR, D_AMPER, EOF
-} t_type;
+int STR_FLAG = 0;
 
-// Stores start of token in src string, and the length of the token with its type.
-typedef struct {
-    t_type type;
-    char* start;
-    int tok_len;
-} token;
-
-token* lex_line(char* src) {
+t_list lex_line(char* src) {
     char* left, *right;
-    token* tokens = malloc(TOK_BUF * sizeof(token));
+    t_list tokens = {(token*) malloc(TOK_BUF * sizeof(token)), 0};
+
+    for(int i = 0; i < strlen(src); i++) {
+        if(src[i] == '&' && peek(src, i) == '&') {
+            add_token(&tokens, D_AMPER, &src[i], 2);
+            i++;
+        } else if(src[i] == '\n')
+            add_token(&tokens, EOL, &src[i], 1);
+
+        // tok buffer
+    }
 
     return tokens;
 }
 
+void add_token(t_list* list, t_type type, char* start, int tok_len) {
+    static int tok_index;
+    token tok = {type, start, tok_len};
+    list->tokens[tok_index] = tok;
+    list->len = ++tok_index;
+}
+
 char peek(char* src, int index) {
     return (index > strlen(src)) ? NULL : src[index];
+}
+
+// Get string from token start to tok_len.
+char* get_literal() {
+
 }
--- src/lex.h
+++ src/lex.h
@@ -4,10 +4,24 @@
 #define LEX_ERR(err) printf("mish: error while tokenizing, %s\n", err);
 #define TOK_BUF 64
 
-typedef struct token;
-typedef enum t_type;
+typedef enum {
+    ARG, STR, D_AMPER, EOL
+} t_type;
 
-token* lex_line(char* src);
+// Stores start of token in src string, and the length of the token with its type.
+typedef struct {
+    t_type type;
+    char* start;
+    int tok_len;
+} token;
+
+typedef struct {
+    token* tokens;
+    int len;
+} t_list;
+
+t_list lex_line(char* src);
 char peek(char* src, int index);
+void add_token(t_list* list, t_type type, char* start, int tok_len);
 
 #endif /* lex.h */
